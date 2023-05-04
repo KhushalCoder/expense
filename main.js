2768f5ad3fe62e452ecada6756e439b90f85b64a
KhushalCoder <79150814+KhushalCoder@users.noreply.github.com>
2023-05-05 00:35:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+document.getElementById('expForm').addEventListener('submit', addTransaction);
+// initial array of transactions, reading from localStorage
+const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+function addTransaction(e) {
+    e.preventDefault();
+  
+    // get type, name, and amount
+    let type = document.getElementById('type').value;
+    let name = document.getElementById('name').value;
+    let amount = document.getElementById('amount').value;
+  
+    if (type != 'chooseOne'
+      && name.length > 0
+      && amount > 0) {
+      const transaction = {
+        type,
+        name,
+        amount,
+        id: transactions.length > 0 ? transactions[transactions.length - 1].id + 1 : 1,
+      }
+  
+      transactions.push(transaction);
+      // localStorage 
+      localStorage.setItem('transactions', JSON.stringify(transactions));
+    }
+  
+    document.getElementById('expForm').reset();
+    showTransactions();
+    updateBalance();
+  }
+  
+  const showTransactions = () => {
+  
+    const transactionTable = document.getElementById('transactionTable');
+  
+    transactionTable.innerHTML = '';
+  
+    for (let i = 0; i < transactions.length; i++) {
+      transactionTable.innerHTML += `
+            <tr>
+                <td>${transactions[i].type}</td>
+                <td>${transactions[i].name}</td>
+                <td>$${transactions[i].amount}</td>
+                <td><a class="deleteButton" onclick="deleteTransaction(${transactions[i].id})">
+                    Delete</td>
+            </tr>
+        `;
+    }
+  }
